af609830afc9359e4a70dde4a4f8649ead229d4e
Aria Rahman <aria.rahman1122@gmail.com>
2022-11-04 02:00:29 +0700

    navbar awal

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,48 @@
-import "./navbar.scss"
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import NightlightRoundOutlinedIcon from "@mui/icons-material/NightlightRoundOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <h1 className="title">Kuli-Coolly Navbar</h1>
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search ..." />
+          <SearchOutlinedIcon />
         </div>
-    )
-}
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <NightlightRoundOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img src="https://i0.wp.com/dianisa.com/wp-content/uploads/2022/08/7.-Gambar-Anime-HD-Keren.jpg?resize=1000%2C580&ssl=1" alt="profile admin"  className="avatar"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,17 +1,16 @@
 import "./sidebar.scss";
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined';
-import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import AssessmentIcon from '@mui/icons-material/Assessment';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import LiveHelpOutlinedIcon from '@mui/icons-material/LiveHelpOutlined';
-import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
-import SettingsSuggestOutlinedIcon from '@mui/icons-material/SettingsSuggestOutlined';
-import AssignmentIndOutlinedIcon from '@mui/icons-material/AssignmentIndOutlined';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import StoreOutlinedIcon from "@mui/icons-material/StoreOutlined";
+import CreditCardOutlinedIcon from "@mui/icons-material/CreditCardOutlined";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LiveHelpOutlinedIcon from "@mui/icons-material/LiveHelpOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import SettingsSuggestOutlinedIcon from "@mui/icons-material/SettingsSuggestOutlined";
+import AssignmentIndOutlinedIcon from "@mui/icons-material/AssignmentIndOutlined";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 const Sidebar = () => {
   return (
@@ -23,61 +22,65 @@ const Sidebar = () => {
       <div className="center">
         <ul>
           <p className="title">MAIN</p>
-            <li>
-              <DashboardIcon/>
-                <span>Dashboard</span>
-            </li>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
           <p className="title">LIST</p>
-            <li>
-              <PersonOutlinedIcon/>
-                <span>Users</span>
-            </li>
-            <p className="title">USEFULL</p>
-            <li>
-              <StoreOutlinedIcon/>
-                <span>Products</span>
-            </li>
-            <li>
-              <CreditCardOutlinedIcon/>
-                <span>Orders</span>
-            </li>
-            <li>
-              <LocalShippingIcon/>
-                <span>Delivery</span>
-            </li>
-            <li>
-              <AssessmentIcon/>
-                <span>Stats</span>
-            </li>
-            <li>
-              <NotificationsNoneIcon/>
-                <span>Notification</span>
-            </li>
-            <p className="title">SERVICE</p>
-            <li>
-              <LiveHelpOutlinedIcon/>
-                <span>System Health</span>
-            </li>
-            <li>
-              <PsychologyOutlinedIcon/>
-                <span>Logs</span>
-            </li>
-            <li>
-              <SettingsSuggestOutlinedIcon/>
-                <span>Setting</span>
-            </li>
-            <p className="title">USER</p>
-            <li>
-              <AssignmentIndOutlinedIcon/>
-                <span>Profile</span>
-            </li>
-            <li>
-              <ExitToAppIcon/>
-                <span>Logout</span>
-            </li>
+          <li>
+            <PersonOutlinedIcon className="icon" />
+            <span>Users</span>
+          </li>
+          <p className="title">USEFULL</p>
+          <li>
+            <StoreOutlinedIcon className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardOutlinedIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <li>
+            <AssessmentIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notification</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <LiveHelpOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsSuggestOutlinedIcon className="icon" />
+            <span>Setting</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AssignmentIndOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span>Logout</span>
+          </li>
         </ul>
       </div>
-      <div className="bottom">Color Option</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   );
 };
