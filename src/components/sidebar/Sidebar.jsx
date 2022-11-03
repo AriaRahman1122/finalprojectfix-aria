5d83a027539299c9acf96a54ff21e12f0cf1938a
Aria Rahman <aria.rahman1122@gmail.com>
2022-11-03 23:52:01 +0700

    tambahan sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,17 @@
 import "./sidebar.scss";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined';
+import CreditCardOutlinedIcon from '@mui/icons-material/CreditCardOutlined';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import LiveHelpOutlinedIcon from '@mui/icons-material/LiveHelpOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import SettingsSuggestOutlinedIcon from '@mui/icons-material/SettingsSuggestOutlined';
+import AssignmentIndOutlinedIcon from '@mui/icons-material/AssignmentIndOutlined';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+
 
 const Sidebar = () => {
   return (
@@ -9,14 +22,58 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
             <li>
+              <DashboardIcon/>
                 <span>Dashboard</span>
             </li>
+          <p className="title">LIST</p>
             <li>
-                <span>Dashboard</span>
+              <PersonOutlinedIcon/>
+                <span>Users</span>
+            </li>
+            <p className="title">USEFULL</p>
+            <li>
+              <StoreOutlinedIcon/>
+                <span>Products</span>
             </li>
             <li>
-                <span>Dashboard</span>
+              <CreditCardOutlinedIcon/>
+                <span>Orders</span>
+            </li>
+            <li>
+              <LocalShippingIcon/>
+                <span>Delivery</span>
+            </li>
+            <li>
+              <AssessmentIcon/>
+                <span>Stats</span>
+            </li>
+            <li>
+              <NotificationsNoneIcon/>
+                <span>Notification</span>
+            </li>
+            <p className="title">SERVICE</p>
+            <li>
+              <LiveHelpOutlinedIcon/>
+                <span>System Health</span>
+            </li>
+            <li>
+              <PsychologyOutlinedIcon/>
+                <span>Logs</span>
+            </li>
+            <li>
+              <SettingsSuggestOutlinedIcon/>
+                <span>Setting</span>
+            </li>
+            <p className="title">USER</p>
+            <li>
+              <AssignmentIndOutlinedIcon/>
+                <span>Profile</span>
+            </li>
+            <li>
+              <ExitToAppIcon/>
+                <span>Logout</span>
             </li>
         </ul>
       </div>
